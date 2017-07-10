6f47eed07aac4f60ee1972130496d298993c4a7e
Lauren Egerton <egertonl@flip3.engr.oregonstate.edu>
2017-07-10 13:13:22 -0700

    Can print two specific columns in .csv file with Boost library

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <sstream>
 #include <iostream>
 #include <string>
+#include <boost/tokenizer.hpp>
 
 int main(int argc, char *argv[]) {
 
@@ -22,25 +23,57 @@ int main(int argc, char *argv[]) {
     auto x = argv[2];
     auto y = argv[3];
     auto numLines = std::stoi(argv[4]);
+    auto xCol = std::stoi(argv[5]);
+    auto yCol = std::stoi(argv[6]);
 
-    /* find way to only parse x, y header columns into vectors
-    std::vector<double> x;
-    std::vector<double> y;
+    fprintf(stdout, "%d \n", xCol);
+    fprintf(stdout, "%d \n", yCol);
+
+     /* find way to only parse x, y header columns into vectors*/
+
+    std::vector<double> xx;
+    std::vector<double> yy;
     std::ifstream inputFile(argv[1]);
 
-    std::string line;
-    
-    while (std::getline(inputFile, line)) {
-        std::stringstream lineStream(line);
-        std::string cell;
+    if (inputFile.is_open()){
+        std::string line;
+        while (std::getline(inputFile, line)){
+            typedef boost::escaped_list_separator<char> Separator;
+            typedef boost::tokenizer<Separator> Tokenizer;
 
-        while (std::getline(lineStream, cell, ',')) {
+            Tokenizer tokenizer(line);
+            int count = 0;
 
+              for (Tokenizer::iterator iter = tokenizer.begin(); (iter != tokenizer.end()) && (count < 9); ++iter)
+            {
+                if ((count == xCol) || (count == yCol))   //prints Col 0 and Col 2 when I put values 0 and 2 here?
+                {
+                    std::cout << *iter;
+                    if (count == xCol)
+                    {
+                        std::cout << "\t";
+                    }
+                }
+                ++count;
+            }
+            std::cout << "\n";
         }
-
     }
 
-    */
+
+    
+    // while (std::getline(inputFile, line)) {
+    //     std::stringstream lineStream(line);
+    //     std::string cell;
+
+    //     while (std::getline(lineStream, cell, ',')) {
+
+    //     }
+
+    // }
+
+    
+
 
 
 
